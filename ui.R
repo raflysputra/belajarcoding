7979a24d47e3901f5e22c9d0165cd1e1d7124c6a
raflysputra <76819641+raflysputra@users.noreply.github.com>
2021-01-03 19:46:17 +0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,3 +1,13 @@
+my_packages = c("shinythemes")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
+
 library(shiny)
 library(shinythemes)
 ui <- fluidPage(theme = shinytheme("slate"),
